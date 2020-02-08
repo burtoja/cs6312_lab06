ffa4d1b285cc5f567de850014600647eeea1c532
J. Allen Burton <jburton6@my.westga.edu>
2020-02-08 18:57:40 -0500

    Added setStructureArea and toString methods to RealEstate class

--- src/edu/westga/cs6312/interfaces/model/RealEstate.java
+++ src/edu/westga/cs6312/interfaces/model/RealEstate.java
@@ -1,7 +1,8 @@
 package edu.westga.cs6312.interfaces.model;
 
 /**
- * This class will model a piece of real estate
+ * This class will model a piece of real estate keeping track of its
+ * location, property area, and structure area
  * 
  * @author J. Allen Burton
  * @version Feb 8, 2020
@@ -16,15 +17,16 @@ public class RealEstate {
 	/**
 	 * 3-parameter constructor for RealEstate objects
 	 *
-	 * @param location
-	 * @param landArea
-	 * @param structureArea
+	 * @param location      the location of the piece of real estate
+	 * @param landArea      the area in square feet of the property lot
+	 * @param structureArea the area in square feet of the structure built on the
+	 *                      property (0 indicates no structure on property)
 	 *
-	 * Precondition: 	location != null 
-	 * 					landArea > 0
-	 * 					structureArea >= 0
+	 *                      Precondition: 	location != null 
+	 *                      				landArea > 0
+	 *                      				structureArea >= 0
 	 *
-	 *  Postcondition:
+	 *                      Postcondition:
 	 */
 	public RealEstate(String location, double landArea, double structureArea) {
 		if (location == null) {
@@ -47,15 +49,48 @@ public class RealEstate {
 	 * built on the property. An object will be created with a structure area of
 	 * zero.
 	 *
-	 * @param location
-	 * @param landArea
+	 * @param location the location of the piece of real estate
+	 * @param landArea the area in square feet of the property lot
 	 *
-	 * Precondition: 	location != null
-	 * 					landArea > 0
+	 *                 Precondition: 	location != null 
+	 *                 					landArea > 0
 	 *
-	 * Postcondition:	a RealEstate object is created with structureArea set to zero
+	 *                 Postcondition: 	a RealEstate object is created with
+	 *                 					structureArea set to zero
 	 */
 	public RealEstate(String location, double landArea) {
 		this(location, landArea, 0);
+	}
+
+	/**
+	 * This method allows the structure area to be set for the object
+	 * 
+	 * @param structureArea
+	 *
+ *                      Precondition: 	structureArea >= 0
+	 *
+ *                      Postcondition: 	object will have new structure area if
+	 *                      			parameter sent is valid
+	 */
+	public void setStructureArea(double structureArea) {
+		if (structureArea < 0) {
+			throw new IllegalArgumentException("Invalid structure area");
+		}
+		this.structureArea = structureArea;
+	}
+
+	/**
+	 * This method will return a string representation of the object variables
+	 * 
+	 * @return string representation of object variables
+	 *
+	 *         			Precondition: none
+	 *
+	 *         			Postcondition: object is not changed
+	 */
+	@Override
+	public String toString() {
+		return "Location: " + this.location + " -- Land Area: " + this.landArea + " sq. ft. -- Structure Area: "
+				+ this.structureArea + " sq. ft.";
 	}
 }
